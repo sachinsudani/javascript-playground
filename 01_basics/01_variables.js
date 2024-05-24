31adf95a2c6b883c00216cdc27a1c085328d11ac
Sachin Sudani <sachinsudani60@gmail.com>
2024-05-24 23:12:03 +0530

    learn: let const and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1444553;
+let accountEmail = "sachin@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+accountEmail = "sachin@mail.com";
+accountPassword = "2121212";
+accountCity = "Rajasthan";
+
+console.log(
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState
+);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
