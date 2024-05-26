ec8bd0f083cb700f128cdeaa06780f3420830a06
Sachin Sudani <sachinsudani60@gmail.com>
2024-05-26 20:20:09 +0530

    learn: basic operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,31 @@
+let value = 3;
+
+// console.log(-value);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+// console.log("Hello" + " Raj");
+
+// console.log(2 + "2");
+// console.log("2" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(2 - "2");
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+// console.log(num1);
+
+let gameCounter = 100;
+
+// gameCounter = gameCounter + 1;
+++gameCounter;
+console.log(gameCounter);
